688ef27364985bf48a2ccb4640db243245ace76d
Daphne Yang <daphne.yang@salesforce.com>
2024-11-22 17:25:03 -0500

    feat: use @salesforce/vscode-service-provider to integrate with XGen LLM

--- src/sendPromptToLLM.ts
+++ src/sendPromptToLLM.ts
@@ -1,3 +1,31 @@
+import * as vscode from 'vscode';
+import { ServiceProvider, ServiceType, AiApiClient, CommandSource } from '@salesforce/vscode-service-provider';
+import * as fs from 'fs';
+
 export const sendPromptToLLM = async (): Promise<void> => {
   console.log('This is the sendPromptToLLM() method');
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    // notificationService.showErrorMessage('No active editor detected');
+    throw Error('No active editor detected');
+  }
+
+  const apiClient = await getAiApiClient();
+  const result = await apiClient.naturalLanguageQuery({
+    prefix: '',
+    suffix: '',
+    input: 'Create an OpenAPI v3 specification for this file',
+    commandSource: CommandSource.NLtoCodeGen,
+    promptId: 'generateOpenAPIv3Specifications'
+  });
+
+  const documentContents = result[0].completion;
+  fs.writeFileSync("documentContents.yaml", documentContents);
+
+  const document = editor.document;
+  console.log('document text = ' + document.getText());
 }
+
+export const getAiApiClient = async (): Promise<AiApiClient> => {
+  return ServiceProvider.getService(ServiceType.AiApiClient);
+};
