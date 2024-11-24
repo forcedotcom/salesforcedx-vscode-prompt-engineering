eee0a456f93add1e99672181ff18c7c1b3533abb
Daphne Yang <daphne.yang@salesforce.com>
2024-11-23 19:57:09 -0500

    feat: use getChatStream() instead of naturalLanguageQuery()

--- src/sendPromptToLLM.ts
+++ src/sendPromptToLLM.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import { ServiceProvider, ServiceType, AiApiClient, CommandSource } from '@salesforce/vscode-service-provider';
 import * as fs from 'fs';
+import { typeGuards, ChatStream } from './typeGuards';
 
 export const sendPromptToLLM = async (): Promise<void> => {
   console.log('This is the sendPromptToLLM() method');
@@ -32,20 +33,100 @@ export const sendPromptToLLM = async (): Promise<void> => {
   console.log('input = ' + input);
 
   const apiClient = await getAiApiClient();
-  const result = await apiClient.naturalLanguageQuery({
-    prefix: '',
-    suffix: '',
-    input: input,
-    commandSource: CommandSource.NLtoCodeGen,
-    promptId: 'generateOpenAPIv3Specifications'
-  });
+  // const result = await apiClient.naturalLanguageQuery({
+  //   prefix: '',
+  //   suffix: '',
+  //   input: input,
+  //   commandSource: CommandSource.NLtoCodeGen,
+  //   promptId: 'generateOpenAPIv3Specifications'
+  // });
 
-  const documentContents = result[0].completion;
+  // const documentContents = result[0].completion;
 
-  // const apiClientStream = await apiClient.getChatStream(input, 'generateOpenAPIv3Specifications');
+  const chatRequestBody = {
+    prompt: input,
+    stop_sequences: ['<|endofprompt|>'],
+    max_tokens: 2048, // Adjust the max_tokens as needed
+    parameters: {
+      command_source: CommandSource.Chat
+    }
+  };
+  console.log('chatRequestBody = ' + JSON.stringify(chatRequestBody));
+  const apiClientStream = await apiClient.getChatStream(chatRequestBody, 'generateOpenAPIv3Specifications');
+  console.log('apiClientStream = ' + JSON.stringify(apiClientStream));
+
+  let documentContents = '';
+  for await (const chunk of apiClientStream) {
+    const { done, text } = processGeneration(chunk);
+    documentContents += text;
+
+    if (done) {
+      break;
+    }
+  }
+
   fs.writeFileSync("documentContents.yaml", documentContents);
 }
 
 export const getAiApiClient = async (): Promise<AiApiClient> => {
   return ServiceProvider.getService(ServiceType.AiApiClient);
 };
+
+/**
+ * @description: parses available text from the generation
+ * and determines if the stream is finished.
+ */
+function processGeneration(chunk: unknown): { done: boolean; text: string } {
+  const processedGeneration = {
+    done: false,
+    text: ''
+  };
+
+  try {
+    if (typeGuards.isChatStream(chunk)) {
+      processedGeneration.done = isTerminationEvent(chunk);
+      processedGeneration.text = getText(chunk);
+      return processedGeneration;
+    } else {
+      processedGeneration.done = true;
+      return processedGeneration;
+    }
+  } catch (error) {
+    processedGeneration.done = true;
+    return processedGeneration;
+  }
+}
+
+/**
+ * Check if this is the last chunk we should process.
+ * Returns true if <|endofprompt|> is in the text and/or
+ * the finish_reason parameter is populated.
+ */
+function isTerminationEvent(chunk: ChatStream): boolean {
+  try {
+    const isTerminationTokenInResponse = chunk.data.generations[0].text.includes('<|endofprompt|>');
+    const doesEventContainFinishReason = chunk.data.generations[0].parameters?.finish_reason;
+
+    return isTerminationTokenInResponse || !!doesEventContainFinishReason;
+  } catch (error) {
+    console.log(error, 'Error determining isTerminationEvent');
+    return true;
+  }
+}
+
+/**
+ * Parse through the chunk to get the text of the generation and
+ * remove <|endofprompt|> token from rawMessage
+ */
+function getText(chunk: ChatStream): string {
+  let text = '';
+  try {
+    const generationText = chunk.data.generations[0].text;
+    text += generationText;
+    text = text.replace('<|endofprompt|>', '');
+    return text;
+  } catch (error) {
+    console.log(error, 'Error getting stream text');
+    return text;
+  }
+}
