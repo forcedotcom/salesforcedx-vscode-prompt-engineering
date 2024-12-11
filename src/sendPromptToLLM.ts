d5c83be9d2599e94e527c67a2f8070fa5b809ef4
Daphne Yang <daphne.yang@salesforce.com>
2024-12-11 12:00:32 -0500

    refactor: move prompt building and calling getChatStream() to XGen block of callLLM()

--- src/sendPromptToLLM.ts
+++ src/sendPromptToLLM.ts
@@ -18,51 +18,7 @@ export const sendPromptToLLM = async (): Promise<void> => {
 
   const userPrompt = 'Generate an OpenAPI v3 specification for my current Apex class. The OpenAPI v3 specification should be in YAML. The paths should be in the format of /{ClassName}/{MethodName} for the @AuraEnabled methods. When you return Id in a SOQL query, it has `type: Id`. For every `type: object`, generate a `#/components/schemas` entry for that object. The method should have a $ref entry pointing to the generated `#/components/schemas` entry. Only include methods that have the @AuraEnabled annotation in the paths of the OpenAPI v3 specification.'
 
-  const systemTag = '<|system|>';
-  const endOfPromptTag = '<|endofprompt|>';
-  const userTag = '<|user|>';
-  const assistantTag = '<|assistant|>';
-
-  const input =
-    `${systemTag}\n${systemPrompt}\n\n${endOfPromptTag}\n${userTag}\n` +
-    userPrompt +
-    `\n\n***Code Context***\n\`\`\`\n` +
-    editorText +
-    `\n\`\`\`\n${endOfPromptTag}\n${assistantTag}`;
-  console.log('input = ' + input);
-
-  const apiClient = await getAiApiClient();
-  // const result = await apiClient.naturalLanguageQuery({
-  //   prefix: '',
-  //   suffix: '',
-  //   input: input,
-  //   commandSource: CommandSource.NLtoCodeGen,
-  //   promptId: 'generateOpenAPIv3Specifications'
-  // });
-
-  // const documentContents = result[0].completion;
-
-  const chatRequestBody = {
-    prompt: input,
-    stop_sequences: ['<|endofprompt|>'],
-    max_tokens: 2048, // Adjust the max_tokens as needed
-    parameters: {
-      command_source: CommandSource.Chat
-    }
-  };
-  console.log('chatRequestBody = ' + JSON.stringify(chatRequestBody));
-  const apiClientStream = await apiClient.getChatStream(chatRequestBody, 'generateOpenAPIv3Specifications');
-  console.log('apiClientStream = ' + JSON.stringify(apiClientStream));
-
-  let documentContents = '';
-  for await (const chunk of apiClientStream) {
-    const { done, text } = processGeneration(chunk);
-    documentContents += text;
-
-    if (done) {
-      break;
-    }
-  }
+  const documentContents = await callLLM(systemPrompt, userPrompt, [editorText], "XGen");
 
   console.log('documentContents = ~' + documentContents + '~');
   fs.writeFileSync("documentContents.yaml", documentContents);
@@ -71,3 +27,52 @@ export const sendPromptToLLM = async (): Promise<void> => {
 export const getAiApiClient = async (): Promise<AiApiClient> => {
   return ServiceProvider.getService(ServiceType.AiApiClient);
 };
+
+const callLLM = async (systemPrompt: string, userPrompt: string, context: [string], llm: string): Promise<string> => {
+  if (llm === "XGen") {
+    const systemTag = '<|system|>';
+    const endOfPromptTag = '<|endofprompt|>';
+    const userTag = '<|user|>';
+    const assistantTag = '<|assistant|>';
+
+    const input =
+    `${systemTag}\n${systemPrompt}\n\n${endOfPromptTag}\n${userTag}\n` +
+    userPrompt +
+    `\n\nThis is the Apex class the OpenAPI v3 specification should be generated for:\n\`\`\`\n` +
+    context[0] +
+    `\n\`\`\`\n${endOfPromptTag}\n${assistantTag}`;
+    console.log('input = ' + input);
+
+    const apiClient = await getAiApiClient();
+
+    const chatRequestBody = {
+      prompt: input,
+      stop_sequences: ['<|endofprompt|>'],
+      max_tokens: 2048, // Adjust the max_tokens as needed
+      parameters: {
+        command_source: CommandSource.Chat
+      }
+    };
+    console.log('chatRequestBody = ' + JSON.stringify(chatRequestBody));
+    const apiClientStream = await apiClient.getChatStream(chatRequestBody, 'generateOpenAPIv3Specifications');
+    console.log('apiClientStream = ' + JSON.stringify(apiClientStream));
+
+    let documentContents = '';
+    for await (const chunk of apiClientStream) {
+      const { done, text } = processGeneration(chunk);
+      documentContents += text;
+
+      if (done) {
+        break;
+      }
+    }
+
+    return documentContents;
+  }
+
+  else if (llm === "OpenAI") {
+    return 'OpenAI case';
+  }
+
+  return 'This shouldn\'t be reached';
+}
